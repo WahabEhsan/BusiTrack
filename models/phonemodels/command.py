19bb0ecceb84fca90f375843cd399ce61a5794a7
Wahab Ehsan <w_ehsan@uncg.edu>
2020-03-04 15:36:37 -0500

    Update command.py included checking
    Update command.py included checking

--- models/phonemodels/command.py
+++ models/phonemodels/command.py
@@ -27,13 +27,31 @@ class Command:
 
     @classmethod
     def add_initiate(cls, msg_list):
-        amount = msg_list[1]
+        user_info = {businesses_short_name: ["Business1", "Business2"]}
+        response = amount_commands_check(msg_list, user_info)
+        if not response:
+            return response
         return "$" + amount + " added."
 
     @classmethod
     def withdraw_initiate(cls, msg_list):
-        amount = msg_list[1]
+        user_info = {businesses_short_name: ["Business1", "Business2"]}
+        response = amount_commands_check(msg_list, user_info)
+        if not response:
+            return response
         return "$" + amount + " withdrawn."
+
+    @classmethod
+    def amount_commands_check(cls, msg_list, user_info):
+        if len(msg_list) != 4:
+            return "Incorrect Format. Type 'h' for help."
+        amount = msg_list[1]
+        business_name = msg_list[3]
+        if not str(amount).isdigit():
+            return "Incorrect Format. Make sure to type command properly. Type 'h' for help."
+        if business_name not in user_info['businesses_short_name']:
+            return "Make sure to type your business name correctly. Type 'h' for help."
+        return True
 
     @classmethod
     def view_initiate(cls):
@@ -45,10 +63,17 @@ class Command:
 
     @classmethod
     def h_initiate(cls):
-        return_msg = "You have typed the help command for BusiTrack.\n\n To use the command" \
-                     " features, follow these instructions: \n\n 1) Add Command: Use this command to keep track of " \
-                     "profits or earnings.\nUsage: Add (amount in digits) to (Your Business).\n\n 2) Withdraw " \
-                     "Command: Use this command to keep track of losses or expenses.\nUsage: Withdraw " \
-                     "(amount in digits) from (Your Business).\n\n 3) View Command: Use this command to show your" \
-                     " added businesses to BusiTrack.\nUsage: View."
+        return_msg = "You have typed the help command for BusiTrack.\n\n " \
+                     "To use the command" \
+                     " features, follow these instructions: \n\n " \
+                     "1) Add Command: Use this command to keep track of " \
+                     "profits or earnings.\n" \
+                     "Usage: Add (amount in digits) to (Your Business).\n\n " \
+                     "2) Withdraw " \
+                     "Command: Use this command to keep track of losses or expenses.\n" \
+                     "Usage: Withdraw " \
+                     "(amount in digits) from (Your Business).\n\n " \
+                     "3) View Command: Use this command to show your" \
+                     " added businesses to BusiTrack.\n" \
+                     "Usage: View."
         return return_msg
