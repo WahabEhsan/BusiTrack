eed71e1062fadae5192501f9c6a108931b654d03
Wahab Ehsan <w_ehsan@uncg.edu>
2020-03-30 15:34:30 -0400

    Update database_translator.py
    added few initail functions

---
+++ controllers/phonecontrollers/database_translator.py
@@ -0,0 +1,16 @@
+import js2py
+
+# Use jsFile as variable to call js functions from example.js
+eval_res, jsFile = js2py.run_file('example.js')
+
+
+def get_one():
+    return
+
+
+def get_all():
+    return
+
+
+def update():
+    return
