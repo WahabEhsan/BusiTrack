37dd68275c645fef08dca005c9d4e2d88d9318c7
clarison608 <bhcandler608@gmail.com>
2020-04-07 04:44:35 -0400

    cleaning up
    Cleaning up

--- models/webmodels/LoginSystem/config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	'url'	: 'mongodb://admin:superpassword@test-shard-00-00-thk0l.mongodb.net:27017,test-shard-00-01-thk0l.mongodb.net:27017,test-shard-00-02-thk0l.mongodb.net:27017/test?ssl=true&replicaSet=test-shard-0&authSource=admin&retryWrites=true&w=majority'
-}
