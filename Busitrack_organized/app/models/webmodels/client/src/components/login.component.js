c43626d371a66e085dd5807ef130b750dc34bef0
clarison608 <42919151+clarison608@users.noreply.github.com>
2020-04-07 04:19:58 -0400

    Add files via upload

---
+++ Busitrack_organized/app/models/webmodels/client/src/components/login.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Login extends Component{
+    render(){
+        return(
+            <div>This is the login page</div>
+        );
+    }
+}
