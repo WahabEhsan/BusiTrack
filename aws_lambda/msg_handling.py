7b6ac24b9b762665d33e6b50660272aac7fd933e
Wahab Ehsan <w_ehsan@uncg.edu>
2020-04-09 18:34:53 -0400

    Added AWS Module folder

---
+++ aws_lambda/msg_handling.py
@@ -0,0 +1,11 @@
+from command import Command
+
+
+def handler(msg_command, number):
+    msg_list = msg_command.split("+")
+    incoming_command = msg_list[0].lower()
+
+    command_object = Command(incoming_command)
+
+    return command_object.check_command(msg_list, number)
+
