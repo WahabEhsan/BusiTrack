593039d2a6d802ce0ce24628f65a604780f731a3
Wahab Ehsan <w_ehsan@uncg.edu>
2020-04-09 01:11:59 -0400

    Third attempt to connect database to phone backend

--- models/phonemodels/phon_dbmodel/phone_db.py
+++ models/phonemodels/phon_dbmodel/phone_db.py
@@ -1,5 +1,7 @@
 import pymongo
 from pymongo import MongoClient
+import datetime
+now = datetime.datetime.now().isoformat()
 
 cluster = MongoClient("mongodb+srv://bt_general:287Y4sdwEh1iVh7s@sandbox-0zyvs.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster['busitrack']
@@ -8,10 +10,16 @@ collection = db['bt_user']
 
 def insert_entry(user_phone, message_list):
     user_info = get_user(user_phone)
-    for busi in user_info['businesses']:
-        if busi['businessName'] == message_list[3]:
-            collection.update_one({'phone': user_phone}, {'$push': {'expenses': {'date': '', 'amount': 0.0}}})
-            return True
+    #for busi in user_info['businesses']:
+     #   if busi['phoneAbrv'] == message_list[3]:
+    #user_info['businesses'][2]
+    if message_list[0].lower() == 'withdraw':
+        amount = float((message_list[1])) * -1
+    else:
+        amount = message_list[1]
+    collection.update_one({'phone': user_phone, 'businesses.phoneAbrv': message_list[3]}, {'$push': {'expenses': {'date': now, 'amount': float(amount)}}})
+
+      #      return True
     return False
 
 
@@ -20,8 +28,6 @@ def get_user(user_phone):
     return user_info
 
 
-#print(get_user('13364024550'))
-#for x in insert_entry("s", "d"):
-#    print(x)
+insert_entry("13364024550", ["withdraw", "100", "to", "Gift"])
 
-insert_entry("13364024550", ["add", "200", "to", "Gift"])
+print(get_user('13364024550'))
