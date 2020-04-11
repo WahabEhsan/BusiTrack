2f3244d2459de05e7bb8185aa51d0b53b98a8759
clarison608 <bhcandler608@gmail.com>
2020-04-11 16:53:19 -0400

    clean up of server junk

--- server.js
+++ server.js
@@ -36,25 +36,8 @@ app.set('view engine', 'ejs');
 
 require('./app/routes.js')(app, passport);
 
-////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////
 
 
 
 app.listen(port);
 console.log('Server running on port: ' + port);
-
-
-
-// require('dotenv').config();
-// const port = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/', function(req, res){
-// 	res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
-// });
-
-// app.listen(port, console.log("Server running at port: " + port))
