67267eb534d87f2235e68512597eb2e3949b9b0c
Wahab Ehsan <w_ehsan@uncg.edu>
2020-04-26 12:54:10 -0400

    Updated View command with correct info
    Updated View command with correct info

--- models/phonemodels/msg_handling.py
+++ models/phonemodels/msg_handling.py
@@ -2,7 +2,7 @@ from command import Command
 
 
 def handler(msg_command, number):
-    msg_list = msg_command.split(" ")
+    msg_list = msg_command.split("+")
     incoming_command = msg_list[0].lower()
 
     command_object = Command(incoming_command)
