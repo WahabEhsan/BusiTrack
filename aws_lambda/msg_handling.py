19d79ed41f055f81ce9e717315c326a2eb68002c
Wahab Ehsan <w_ehsan@uncg.edu>
2020-04-27 11:43:11 -0400

    Checks for extra space at the end of msg
    Checks for extra space at the end of msg

--- aws_lambda/msg_handling.py
+++ aws_lambda/msg_handling.py
@@ -2,6 +2,9 @@ from command import Command
 
 
 def handler(msg_command, number):
+    if msg_command.endswith("+"):
+        msg_command = msg_command[:-1]
+
     msg_list = msg_command.split("+")
     incoming_command = msg_list[0].lower()
 
